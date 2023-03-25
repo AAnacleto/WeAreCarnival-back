ae6403d460735840adf520db9c60a2f95984e89a
Allysson Oliveira <allysson.oliveira@capgemini.com>
2023-03-25 16:18:34 -0300

    first commit

---
+++ src/main/java/wearecarnival/com/WearecarnivalApplication.java
@@ -0,0 +1,13 @@
+package wearecarnival.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearecarnivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearecarnivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wearecarnival/com/WearecarnivalApplicationTests.java
@@ -0,0 +1,13 @@
+package wearecarnival.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearecarnivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
