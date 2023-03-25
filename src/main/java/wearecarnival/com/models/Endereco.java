8937968f4bf9d8e0aef7122aadf8984d24bf2530
Allysson Oliveira <allysson.oliveira@capgemini.com>
2023-03-25 17:53:47 -0300

    criado as models de eventos e endereco

---
+++ src/main/java/wearecarnival/com/models/Endereco.java
@@ -0,0 +1,37 @@
+package wearecarnival.com.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Endereco implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String nomeRua;
+
+    @Column(unique = true, nullable = false)
+    private String numero;
+
+    @Column(unique = true, nullable = false)
+    private String bairro;
+
+    @Column(unique = true, nullable = false)
+    private String cidade;
+
+    @Column(unique = true, nullable = true)
+    private String pontoReferencia;
+
+}
---
+++ src/main/java/wearecarnival/com/models/Eventos.java
@@ -0,0 +1,39 @@
+package wearecarnival.com.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Eventos implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+
+    @Lob
+    @Column(columnDefinition = "BLOB", nullable = false)
+    private byte[] imagem;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @Column()
+    private boolean favoritos;
+}
